dbcbd367df0e24408b023d17bd9aa6066cf40ec9
yash sharma <yashsharma@yashs-MacBook-Pro.local>
2020-06-27 15:50:42 +0530

    added models for account, accountType and customer, also added sessionFactory

---
+++ src/main/java/com/blupplup/bank/Daos/Impl/AccountDaoImpl.java
@@ -0,0 +1,32 @@
+package com.blupplup.bank.Daos.Impl;
+
+import com.blupplup.bank.Daos.AccountDao;
+import com.blupplup.bank.models.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+@Component
+public class AccountDaoImpl implements AccountDao {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+
+    @Override
+    public List<Account> getAllAccountList() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+
+        CriteriaQuery<Account> criteriaQuery = criteriaBuilder.createQuery(Account.class);
+        List<Account> accounts = session.createQuery(criteriaQuery).getResultList();
+
+        return accounts;
+    }
+}
---
+++ src/main/java/com/blupplup/bank/models/Customer.java
@@ -0,0 +1,80 @@
+package com.blupplup.bank.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String panNumber;
+    private String mobile;
+    @OneToMany(
+            mappedBy = "customer",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    private Set<Account> accounts;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPanNumber() {
+        return panNumber;
+    }
+
+    public void setPanNumber(String panNumber) {
+        this.panNumber = panNumber;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+}
